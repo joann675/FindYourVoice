1ffcfc74caf02e9832838eb62b1cd52603effd09
joann675 <joann675@yahoo.com>
2019-03-05 23:07:46 -0500

    Added passport for login

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var session = require("express-session");
+
+var passport = require("./config/passport");
 
 var PORT = process.env.PORT || 3000;
 
@@ -12,6 +15,11 @@ app.use(express.static("public"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 // Import routes and give the server access to them.

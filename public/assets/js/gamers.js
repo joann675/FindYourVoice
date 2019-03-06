216807c85e51bc4832dd5b9e1f2c644ba7212e0e
Dina <counting1121@yahoo.com>
2019-03-05 21:07:45 -0500

    signin form

--- public/assets/js/gamers.js
+++ public/assets/js/gamers.js
@@ -1,29 +1,63 @@
 
- // Set up of creation of new burger submit button
-  $("form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+// Set up of creation of a submit button
+$(document).ready(function () {
+
+  var signinForm = $("form.signup");
+  var nameInput = $("input#name");
+  var emailInput = $("input#email-input");
+  var dobInput = $("input#dob");
+  var passwordInput = $("input#password-input");
+  var privacyInput = $("select#privacySetting");
+  var genreInput = $("select#genreList");
+  var platformInput = $("select#platformList");
+
+  signinForm.on("submit", function (event) {
     event.preventDefault();
-
-    var formData = $(form).serialize();
-
-    // Send the POST request.
-    $.ajax("/", {
-      type: "POST",
-      url: $("form").attr('action'),
-      data: formData
-    }).then(
-      function() {
-        console.log("checking submit");
-        $(formMessages).removeClass("error");
-       $(formMessages).addClass("success");
-
-       $(formMessages).text(response);
-
-       // Clear the form.
-       $('#name').val('');
-       $('#email').val('');
-       $('#message').val('');
-      }
-    );
+    var userData = {
+      name: nameInput.val().trim(),
+      email: emailInput.val().trim(),
+      dob: dobInput.val().trim(),
+      password: passwordInput.val().trim(),
+      privacySetting: privacyInput.val().trim(),
+      genreList: genreInput.val().trim(),
+      platform: platformInput.val().trim()
+    };
+    signInUser(userData.name, userData.email, userData.dob, userData.password,
+      userData.privacySetting, userData.genreList, userData.platform);
+    nameInput.val("");
+    emailInput.val("");
+    dobInput.val("");
+    passwordInput.val("");
+    privacyInput.val("");
+    genreInput.val("");
+    platformInput.val("");
   });
+
+
+  // Send the POST request.
+  function signInUser(name, email, dob, password, privacySetting, genreList, platform) {
+    $.post("/api/users", {
+      name: name,
+      email: email,
+      dob: dob,
+      password: password,
+      privacySetting: privacySetting,
+      genreList: genreList,
+      platform: platform
+    }).then(function (data) {
+      window.location.replace(data);
+    }).catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+
+  }
+
+
+});
+
+
+
 

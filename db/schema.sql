c20a227a6bba801ad776ffea768eef17518b45b6
joann675 <joann675@yahoo.com>
2019-03-05 20:37:27 -0500

    Updated data model

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+
+*/
+
+-- Create the database seqBurgers_db and specified it for use.
+DROP DATABASE IF EXISTS `gamecentralDev_db`;
+CREATE DATABASE `gamecentralDev_db`;
+

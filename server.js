e4ff3e06df642acfc6ec84e45a87f35a9d42e948
unknown <vishalsindhu@aim.com>
2019-03-07 19:02:27 -0500

    stash

--- server.js
+++ server.js
@@ -27,12 +27,8 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 
-<<<<<<< HEAD
-db.sequelize.sync({force: false}).then(function() {
-=======
 
 db.sequelize.sync({force: true}).then(function() {
->>>>>>> 81ae957f31d43f2565ac3943852c707406a577ff
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });

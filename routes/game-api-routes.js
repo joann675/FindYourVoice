96963332d53f5eff6e930b407e20fada2a422312
unknown <vishalsindhu@aim.com>
2019-03-07 20:56:25 -0500

    fixed axios call

--- routes/game-api-routes.js
+++ routes/game-api-routes.js
@@ -1,25 +1,24 @@
 var searched = document.getElementById("searched").value;
 axios({
-    url: "https://api-v3.igdb.com/search",
+    url: "https://api-v3.igdb.com/search?="+searched+"&limit=10",
     method: 'POST',
     headers: {
         'Accept': 'application/json',
         'user-key': f254966f3b07955f5a90d3fa27f17250
     },
     data: "fields alternative_name,character,collection,company,description,game,name,person,platform,popularity;search;"
-     + searched + "limit 10;"
-  })
+       })
     .then(response => {
         console.log(response.data);
         axios({
-            url: "https://api-v3.igdb.com/artworks",
+            url: "https://api-v3.igdb.com/artworks?="+searched+"&limit=2",
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'user-key': f254966f3b07955f5a90d3fa27f17250
             },
             data: "fields alpha_channel,animated,game,height,image_id,url,width;"
-            + searched + "limit 2;"
+
           })
             .then(response => {
                 console.log(response.data);

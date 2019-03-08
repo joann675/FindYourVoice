bd5561f2022af40cfe83ef6e03e3b20331b4e753
joann675 <42893916+joann675@users.noreply.github.com>
2019-03-07 21:36:20 -0500

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -24,9 +24,9 @@ module.exports = function (app) {
   });
 
   app.get("/api/games/:userid", function (req, res) {
-    
+
     db.UserGameStatuses.findAll({
-      include: [db.Games ],
+      include: [db.Games],
       where: {
         UserId: req.params.userid
       }
@@ -37,9 +37,9 @@ module.exports = function (app) {
 
 
   app.get("/api/games/:userid/:playState", function (req, res) {
-    
+
     db.UserGameStatuses.findAll({
-      include: [db.Games ],
+      include: [db.Games],
       where: {
         UserId: req.params.userid,
         state: req.params.playState
@@ -57,7 +57,7 @@ module.exports = function (app) {
     // findAll returns all entries for a table when used with no options
     db.Users.findAll({}).then(function (users) {
       // We have access to the todos as an argument inside of the callback function
-      res.json(main);
+      res.json(users);
     });
 
   });
@@ -81,13 +81,19 @@ module.exports = function (app) {
       res.json(user);
     });
 
-  }); 
+  });
 
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
-    res.json("/index");
+    db.Users.findOne({
+      where: {
+        name: req.body.username
+      }
+    }).then(function (results) {
+      res.json(results);
+    });
   });
 
 
@@ -100,8 +106,10 @@ module.exports = function (app) {
         title: req.body.title,
         releaseDate: req.body.releaseDate
       },
-      defaults: {platforms: req.body.platforms,
-                agerating: req.body.agerating}
+      defaults: {
+        platforms: req.body.platforms,
+        agerating: req.body.agerating
+      }
     }).then(function (results) {
 
       db.UserGameStatuses.create({

b6ccf988688e7cdca39beeaf0d688ed9df201e22
unknown <vishalsindhu@aim.com>
2019-03-12 17:15:38 -0400

    c

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -34,28 +34,29 @@ $(document).ready(function(){
     $("#serchbtn").on("click", function(event){
       console.log("I am clicked");
       event.preventDefault()
-      let search = $("#searched").val().trim()
+      let userSearch = $("#searched").val().trim()
      let axios = axios.get({
-        url: "https://api-v3.igdb.com/search?"+search + "&limit=10",
+        url: "https://api-v3.igdb.com/games/",
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'user-key': f254966f3b07955f5a90d3fa27f17250
         },
-        data: "fields alternative_name,character,collection,company,description,game,name,person,platform,popularity;search;"
-         + searched + "limit 10;"
+       body: {
+         'search': userSearch,  
+         'fields': "age_ratings,genres,platforms,popularity;"
+       }
       })
         .then(response => {
             console.log(response.data);
             axios.get({
-                url: "https://api-v3.igdb.com/artworks?"+search + "&limit=2",
+                url: "https://api-v3.igdb.com/artworks",  
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'user-key': f254966f3b07955f5a90d3fa27f17250
                 },
                 data: "fields alpha_channel,animated,game,height,image_id,url,width;"
-                + searched + "limit 2;"
               })
                 .then(response => {
                     console.log(response.data);
